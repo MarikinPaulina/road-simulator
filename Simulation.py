b5f9a4750f8743bf0d3d1c888e0c955643878852
MarikinPaulina <felicitaspura@gmail.com>
2019-08-28 18:10:39 +0200

    end off cleaning + debug

--- Simulation.py
+++ Simulation.py
@@ -1,87 +1,16 @@
-def random_vertex(l):
-    import numpy as np
-
-    x, y = np.random.random(2)*2*l-l
-    return np.array([x,y])
-
-def find_segments(vertices, segments, d, epsilon):
-    from scipy.spatial import ckdtree
-
-    active_segments = [] #segmenty które będą się rozrastać
-    segments_vertices = [] #krawędzie do których będzie rozrastać się segment na odpowiednim miejscu powyżej
-    tree = ckdtree.cKDTree(segments)
-    for i in range(len(vertices)-1,-1,-1):
-        dist, nearest_segments = tree.query(vertices[i])
-        if dist < d:
-            del vertices[i]
-    for i in range(len(vertices)):
-        dist, nearest_segments = tree.query(vertices[i],5)
-        close = ((dist-dist[0])<epsilon)
-        for s in nearest_segments[close]:
-            if segments[s] in active_segments:
-                index = active_segments.index(segments[s])
-                segments_vertices[index].extend([i])
-            else:
-                active_segments.append(segments[s])
-                segments_vertices.append([i])
-
-    return active_segments, segments_vertices
-
-
-def segment_adding(M:int, active_vertices, active_segments, segments_vertices, segments,d):
-    for m in range(M):
-        for i in range(len(active_segments)-1, -1, -1):
-            dist_x = 0
-            dist_y = 0
-            r_list = []
-            for j in range(len(segments_vertices[i])):
-                x = active_vertices[segments_vertices[i][j]][0] - active_segments[i][0]
-                y = active_vertices[segments_vertices[i][j]][1] - active_segments[i][1]
-                dist_x += x
-                dist_y += y
-                r_list.append((x**2 + y**2)**0.5)
-            r = (dist_x**2 + dist_y**2)**0.5
-            if r > d:
-                new_seg = (active_segments[i][0]+dist_x*d/r,active_segments[i][1]+dist_y*d/r)
-#                 print(((dist_x/r*d)**2+(dist_y/r*d)**2)**0.5)
-                segments.append(new_seg)
-#                 visual.append([active_segments[i],new_seg])
-                active_segments[i] = new_seg
-            else:
-                closest_id = r_list.index(min(r_list))
-                vertex = segments_vertices[i][closest_id]
-                del segments_vertices[i][closest_id]
-                if len(segments_vertices[i]) == 0:
-                    del segments_vertices[i]
-                    del active_segments[i]
-                else:
-                    x = active_vertices[vertex][0] - active_segments[i][0]
-                    y = active_vertices[vertex][1] - active_segments[i][1]
-                    r = r_list[closest_id]
-                    new_seg = (active_segments[i][0]+x*d/r,active_segments[i][1]+y*d/r)
-                    segments.append(new_seg)
-#                     visual.append([active_segments[i],new_seg])
-                    active_segments.append(new_seg)
-                    segments_vertices.append([vertex])
-
-#                         segments_vertices.remove(segments_vertices[i])
-#                         active_segments.remove(active_segments[i])
+from tqdm.autonotebook import tqdm
+import numpy as np
+from scipy.spatial import ckdtree
 
 def run(N,M,Frames,l,d,epsilon):
-    from tqdm.autonotebook import tqdm
-    import numpy as np
-    from scipy.spatial import ckdtree
-
-    segments = [(0,0)]
-    active_vertices = []
-    animation_vertices = []
-    animation_segments = []
+    segments, active_vertices, animation_vertices, animation_segments = reset()
     for n in tqdm(range(N)):
         active_vertices.append(random_vertex(l))
         active_segments, segments_vertices = find_segments(active_vertices, segments,d,epsilon)
         for f in range(Frames):
-            segment_adding(M,active_vertices,active_segments,segments_vertices,segments,d)
-            if len(active_segments) != 0:
+            L = len(segments)
+            active_segments, segments_vertices = segments_adding(M,active_vertices,active_segments,segments_vertices,segments,d,epsilon)
+            if L != len(segments):
                 animation_vertices.append(np.array(active_vertices))
                 animation_segments.append(np.array(segments))
 
@@ -89,6 +18,97 @@ def run(N,M,Frames,l,d,epsilon):
     animation_vertices.append(np.array(active_vertices))
     animation_segments.append(np.array(segments))
 
-    # active_vertices = np.vstack(active_vertices).T
     segments = np.vstack(np.array(segments)).T
     return [animation_segments,animation_vertices]
+
+def reset():
+    return [(0,0)], [], [], []
+
+def random_vertex(l):
+    x, y = np.random.random(2)*2*l-l
+    return np.array([x,y])
+
+def find_segments(vertices, segments, d, epsilon):
+    active_segments = [] #segmenty które będą się rozrastać
+    segments_vertices = [] #krawędzie do których będzie rozrastać się segment na odpowiednim miejscu powyżej
+    tree = ckdtree.cKDTree(segments)
+    for i in range(len(vertices)-1,-1,-1):
+        dist, nearest_segments = tree.query(vertices[i])
+        if dist < d:
+            vertices.pop(i)
+    for i in range(len(vertices)):
+        find_segment(i, tree, epsilon, segments, vertices, active_segments, segments_vertices, )
+    return active_segments, segments_vertices
+
+def find_segment(i, tree, epsilon, segments, vertices, active_segments, segments_vertices, ):
+    dist, nearest_segments = tree.query(vertices[i],5)
+    close = ((dist-dist[0])<epsilon)
+    for s in nearest_segments[close]:
+        if segments[s] in active_segments:
+            index = active_segments.index(segments[s])
+            segments_vertices[index].extend([i])
+        else:
+            active_segments.append(segments[s])
+            segments_vertices.append([i])
+
+def segments_adding(M:int, active_vertices, active_segments, segments_vertices, segments,d,epsilon):
+    recalibrate = False
+    for m in range(M):
+        if recalibrate:
+            # print("Init") #Tu jest coś dziwnego
+            active_segments, segments_vertices = find_segments(active_vertices, segments,d,epsilon)
+            recalibrate = False
+        for i in range(len(active_segments)-1, -1, -1):
+            # TODO wyrzucić to stąd
+            recalibrate = segment_adding(i, active_vertices, active_segments, segments_vertices, segments, d)
+
+    return active_segments, segments_vertices
+
+def segment_adding(i, active_vertices, active_segments, segments_vertices, segments, d, ):
+
+    r, r_list, dist_x, dist_y = compute_dist(i, active_vertices, active_segments, segments_vertices, d, )
+    recalibrate = False
+    if r > d:
+        new_seg = (active_segments[i][0]+dist_x,active_segments[i][1]+dist_y)
+        for j in range(len(r_list)):
+            x = active_vertices[segments_vertices[i][j]][0] - new_seg[0]
+            y = active_vertices[segments_vertices[i][j]][1] - new_seg[1]
+            if (x**2+y**2) > r_list[j]**2:
+                recalibrate = True
+                break
+        if not recalibrate:
+            segments.append(new_seg)
+            active_segments[i] = new_seg
+            return recalibrate
+    recalibrate = True
+    recalibration(i ,r_list, segments_vertices, active_segments, active_vertices, segments, d, )
+    return recalibrate
+
+def compute_dist(i, active_vertices, active_segments, segments_vertices, d, ):
+    dist_x = 0
+    dist_y = 0
+    r_list = []
+    for j in range(len(segments_vertices[i])):
+        x = active_vertices[segments_vertices[i][j]][0] - active_segments[i][0]
+        y = active_vertices[segments_vertices[i][j]][1] - active_segments[i][1]
+        r = (x**2 + y**2)**0.5
+        dist_x += x
+        dist_y += y
+        r_list.append(r)
+    r = (dist_x**2 + dist_y**2)**0.5
+    dist_x = dist_x*d/r
+    dist_y = dist_y*d/r
+    return r, r_list, dist_x, dist_y
+
+def recalibration(i, r_list, segments_vertices, active_segments, active_vertices, segments, d, ):
+    closest_id = r_list.index(min(r_list))
+    vertex = segments_vertices[i].pop(closest_id)
+    if len(segments_vertices[i]) == 0:
+        segments_vertices.pop(i)
+        active_segments.pop(i)
+    else:
+        x = active_vertices[vertex][0] - active_segments[i][0]
+        y = active_vertices[vertex][1] - active_segments[i][1]
+        r = r_list[closest_id]
+        new_seg = (active_segments[i][0]+x*d/r,active_segments[i][1]+y*d/r)
+        segments.append(new_seg)
--- Visual.py
+++ Visual.py
@@ -18,9 +18,10 @@ def animated_frames(animation_segments, animation_vertices):
         if frame_vertices.size > 0:
             scatterplot = ax.scatter(frame_vertices[:, 0], frame_vertices[:, 1])
         # TODO optimize the line below - done?
-        lines = ax.scatter(frame_segments[0], frame_segments[1],0.5,'k')
+        lines = ax.scatter(frame_segments[0], frame_segments[1],0.1,'k')
 
         #         lines = [ax.plot(segment[:, 0], segment[:, 1]) for segment in frame_segments]
+        ax.axis('equal')
 
         title = ax.set_title(f"Iteration {f}/{len(animation_segments) - 1}")
         fig.canvas.draw()
@@ -45,32 +46,36 @@ def animated_frames(animation_segments, animation_vertices):
     #     ipywidgets.HBox([play, slider])
     # 1.png
     return interact(update, f=play);
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from tqdm.auto import tqdm as tqdm
+import multiprocessing
+
+# def make_frame(f, animation_segments_f, animation_vertices_f, folder, len_animation_segments):
+
+def make_frame(tuple_of_arguments):
+    f, animation_segments_f, animation_vertices_f, folder, len_animation_segments = tuple_of_arguments
+    fig, ax = plt.subplots()
+    frame_segments = animation_segments_f.T
+    frame_vertices = animation_vertices_f
+    if frame_vertices.size > 0:
+        scatterplot = ax.scatter(frame_vertices[:, 0], frame_vertices[:, 1])
+    lines = ax.scatter(frame_segments[0], frame_segments[1], 0.25, 'k')
+    title = ax.set_title(f"Iteration {f}/{len_animation_segments - 1}")
+    fig.savefig(os.path.join(folder, f"{f:06}.png"), dpi=400)
+    plt.close(fig)
 
 
-def save_frames(animation_segments, animation_vertices, folder):
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import os
-    from tqdm.auto import tqdm as tqdm
-    import os
-    import psutil
-    import multiprocessing
-    process = psutil.Process(os.getpid())
-
-    def make_frame(f, animation_segments_f, animation_vertices_f, folder, len_animation_segments):
-        fig, ax = plt.subplots()
-        frame_segments = animation_segments_f.T
-        frame_vertices = animation_vertices_f
-        if frame_vertices.size > 0:
-            scatterplot = ax.scatter(frame_vertices[:, 0], frame_vertices[:, 1])
-        lines = ax.scatter(frame_segments[0], frame_segments[1], 0.5, 'k')
-        title = ax.set_title(f"Iteration {f}/{len_animation_segments - 1}")
-        fig.savefig(os.path.join(folder, f"{f:06}.png"), dpi=400)
-
+def save_frames(animation_segments, animation_vertices, folder,processes):
     os.makedirs(folder, exist_ok=True)
 
-    for f in tqdm(range(len(animation_segments))):
-        proc = multiprocessing.Process(target=make_frame, args=(f, animation_segments[f], animation_vertices[f], folder, len(animation_segments)))
-        proc.daemon=True
-        proc.start()
-        proc.join()
+    arguments = []
+    for f in range(len(animation_segments)):
+        arguments.append((f, animation_segments[f], animation_vertices[f], folder, len(animation_segments)))
+
+    with multiprocessing.Pool(processes) as p:
+        outs = list(tqdm(p.imap(make_frame, arguments), total=len(arguments)))
+
+    # for f in tqdm(range(len(animation_segments))):
+    #     make_frame(f, animation_segments[f], animation_vertices[f], folder, len(animation_segments))
