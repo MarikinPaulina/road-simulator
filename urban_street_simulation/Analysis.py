db8a4ba481be8e5565986156dfdabbafb4965f7e
Paulina Marikin <felicitaspura@gmail.com>
2019-12-11 18:24:11 +0100

    analysis functions - under construction

--- urban_street_simulation/Analysis.py
+++ urban_street_simulation/Analysis.py
@@ -2,6 +2,12 @@ from tqdm.autonotebook import tqdm
 import numpy as np
 from scipy.spatial import ckdtree
 import shapely
+from smallestenclosingcircle import make_circle
+import matplotlib.pyplot as plt
+from pathlib import Path
+import re
+import os
+from urban_street_simulation.Simulation import load_data
 
 
 def make_polygons(segments, d):
@@ -22,3 +28,14 @@ def flat_list(lista):
         for item in sublist:
             out.append(item)
     return out
+
+
+def avrage_segments(path, name):
+    regex = fr'.*{name}.*'
+    all_files = [file for file in os.listdir(path) if os.path.is_file(file)]
+    data_files = [path / file for file in all_files if re.match(regex, file)]
+    num_datesets = len(data_files)
+    num_segments = 0
+    for file in data_files:
+        segments = load_data(file)[0]
+        num_segments += len(segments)
